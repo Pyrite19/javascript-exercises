ba01c4a4bfccdd178314870e0e6224eda7698619
BuildTools <unconfigured@null.spigotmc.org>
2022-04-23 18:51:46 -0500

    Challenge 4 complete.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(a, ...remove) {
+    const array = a;
+    const newArray = [];
+    for (i in array) {
+        if (!remove.includes(array[i])) {
+            newArray.push(array[i])
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line

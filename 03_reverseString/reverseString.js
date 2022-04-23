b0017ff99d99bd7df50d47f43783cb95203ff60c
BuildTools <unconfigured@null.spigotmc.org>
2022-04-23 15:43:36 -0500

    Exercise 3 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+backString = ""
+for (let i = (string.length - 1); i > -1; i--){
+    backString += string[i]
+}
+return backString
 };
 
 // Do not edit below this line

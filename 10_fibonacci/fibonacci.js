a37eb5f6e5eeef7e44a850ccac3907601048e2b0
BuildTools <unconfigured@null.spigotmc.org>
2022-04-25 13:58:34 -0500

    Fibonacci exercise complete.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+number = Number(input)
+fSequence = [1, 1]
+if (0 > number) {
+    return "OOPS"
+}
+else {
+    if (number > fSequence.length) {
+        for (let i = 1; i < number - 1;i++) {
+            fSequence.push((fSequence[i - 1]) + fSequence[i])
+            console.log(fSequence)
+        }
+    }
+    return fSequence[(fSequence.length - 1)]
+}
 };
 
 // Do not edit below this line

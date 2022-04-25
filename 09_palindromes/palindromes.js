7d0b6533ae613d3dc33abf8c45599d2961d3dc2c
BuildTools <unconfigured@null.spigotmc.org>
2022-04-25 13:35:16 -0500

    Palindromes challenge complete.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+formString = String(string);
+formString = formString.toLowerCase();
+formString = formString.replace(/[^a-z]/g, "");
+backwardsString = reverse(formString);
+if (backwardsString == formString) {
+    return true;
+}
+else {
+    return false;
+}
 };
+
+function reverse(str) {
+let backString = ""
+    for (let i = (str.length - 1); i > -1; i--) {
+        backString += str.charAt(i)
+    }
+    return backString;
+}
 
 // Do not edit below this line
 module.exports = palindromes;

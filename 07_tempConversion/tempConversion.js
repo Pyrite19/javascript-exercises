eaa2629d62ffc6ace74dc12711a70f85fa4c6296
BuildTools <unconfigured@null.spigotmc.org>
2022-04-24 19:04:42 -0500

    tempConversion complete.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+convertedTemp = Number((((temp - 32) * 5) / 9))
+convertedTemp = Math.round(convertedTemp * 10) / 10
+return convertedTemp;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+convertedTemp = Number((temp * (9 / 5)) + 32)
+console.log(convertedTemp)
+convertedTemp = Math.round(convertedTemp * 10) / 10
+return convertedTemp;
 };
 
 // Do not edit below this line

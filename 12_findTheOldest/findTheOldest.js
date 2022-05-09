ff74b422031ba8e399c20cd7a5e1efc8af9bc1ff
Timothy Davidson <timd1919@gmail.com>
2022-05-09 15:42:56 -0500

    Exercise 12 complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+	return array.reduce((oldest, currentPerson) => {
+		oldestAge = AgeSolver(oldest.yearOfBirth, oldest.yearOfDeath);
+		currentAge = AgeSolver(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+		if (oldestAge < currentAge) {
+			return currentPerson;
+		} else {
+			return oldest;
+		}
+	});
+};
 
+const AgeSolver = function (birth, death) {
+	if (death == undefined) {
+		death = new Date().getFullYear();
+	}
+	return death - birth;
 };
 
 // Do not edit below this line
